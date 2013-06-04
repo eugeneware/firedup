40d1410042647b92d9724bc4b9651d81cfa1e6c9
Eugene Ware <eugene@noblesamurai.com>
2013-06-04 21:25:26 +1000

    refactor out watch function

--- test/follow.js
+++ test/follow.js
@@ -4,9 +4,10 @@ var expect = require('chai').expect
   , levelup = require('levelup')
   , bytewise = require('byteup')()
   , sublevel = require('level-sublevel')
-  , livestream = require('level-live-stream')
   , _ = require('underscore')
-  , levelplus = require('levelplus');
+  , levelplus = require('levelplus')
+  , async = require('async')
+  , firedup = require('../lib/firedup');
 
 describe('firedup', function () {
   var dbPath = path.join(__dirname, '..', 'data', 'test');
@@ -17,6 +18,7 @@ describe('firedup', function () {
     db = levelup(dbPath, { keyEncoding: 'bytewise', valueEncoding: 'json' },
       function (err) {
         db = levelplus(db);
+        db = firedup(db);
         db = sublevel(db);
         done();
       });
@@ -31,16 +33,26 @@ describe('firedup', function () {
 
     doInsert(0, 10, function (err) {
       if (err) return done(err);
-      livestream(db, {
-          start: ['users', -Infinity],
-          end: ['users', Infinity],
-          old: true
-        }).on('data', function (data) {
+      db.urlWatch('users')
+        .on('data', function (data) {
           --count || done();
         });
 
-      doInsert(0, 10);
+      doInsert(0, 10, function (err) {
+        if (err) return done(err);
+        doDelete(0, 10);
+      });
     });
+
+    function doDelete(offset, n, cb) {
+      var count = n;
+      _.range(offset, offset + n).forEach(function (i) {
+        db.del(['users', i], function (err) {
+          if (err) return cb(err);
+          --count || cb && cb();
+        });
+      });
+    }
 
     function doInsert(offset, n, cb) {
       var count = n;
