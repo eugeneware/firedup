c1f2daf6727734574699580c4d806753d37e779a
Eugene Ware <eugene@noblesamurai.com>
2013-06-09 16:37:31 +1000

    protect from minimisation issues

--- public/script/socket.js
+++ public/script/socket.js
@@ -1,55 +1,57 @@
-angular.module('socket.io', []).factory('socket', function ($rootScope, $q) {
-  var d = $q.defer();
-  var socket = io.connect();
-  var fns = [];
-  var rpcId = 0;
-  var rpcCbs = {};
-  var ngSock = {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
+angular.module('socket.io', []).factory('socket', ['$rootScope', '$q',
+  function ($rootScope, $q) {
+    var d = $q.defer();
+    var socket = io.connect();
+    var fns = [];
+    var rpcId = 0;
+    var rpcCbs = {};
+    var ngSock = {
+      on: function (eventName, callback) {
+        socket.on(eventName, function () {
+          var args = arguments;
+          $rootScope.$apply(function () {
             callback.apply(socket, args);
-          }
+          });
         });
-      })
-    },
-    apiCall: function (methodName) {
-      var args = Array.prototype.slice.call(arguments);
-      args.shift();
-      var cb = args.pop();
-      rpcId++;
-      var msgId = rpcId;
-      ngSock.emit('apiCall', {
-        id: msgId,
-        name: methodName,
-        data: args
+      },
+      emit: function (eventName, data, callback) {
+        socket.emit(eventName, data, function () {
+          var args = arguments;
+          $rootScope.$apply(function () {
+            if (callback) {
+              callback.apply(socket, args);
+            }
+          });
+        })
+      },
+      apiCall: function (methodName) {
+        var args = Array.prototype.slice.call(arguments);
+        args.shift();
+        var cb = args.pop();
+        rpcId++;
+        var msgId = rpcId;
+        ngSock.emit('apiCall', {
+          id: msgId,
+          name: methodName,
+          data: args
+        });
+        rpcCbs[msgId] = cb;
+      },
+      remote: {}
+    };
+    ngSock.on('remote', function (fns_) {
+      fns = fns_;
+      fns.forEach(function (fnName) {
+        ngSock.remote[fnName] = ngSock.apiCall.bind(null, fnName);
       });
-      rpcCbs[msgId] = cb;
-    },
-    remote: {}
-  };
-  ngSock.on('remote', function (fns_) {
-    fns = fns_;
-    fns.forEach(function (fnName) {
-      ngSock.remote[fnName] = ngSock.apiCall.bind(null, fnName);
+      d.resolve(ngSock);
     });
-    d.resolve(ngSock);
-  });
-  ngSock.on('apiResponse', function (err, id, data) {
-    var cb = rpcCbs[id];
-    delete rpcCbs[id];
-    cb(err, data);
-  });
+    ngSock.on('apiResponse', function (err, id, data) {
+      var cb = rpcCbs[id];
+      delete rpcCbs[id];
+      cb(err, data);
+    });
 
-  return d.promise;
-});
+    return d.promise;
+  }
+]);
